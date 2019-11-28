37b0a06f3e2ee38e87616cb7468b28a2bb98f693
Gihyeon Yang <dexterastin@gmail.com>
2019-11-28 21:28:18 +0900

    feat: public

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const indexRouter = require('./routes/index');
 const evaluationRouter = require('./routes/evaluation');
 
 const app = express();
-
+console.log(path.join(__dirname, 'public'))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
